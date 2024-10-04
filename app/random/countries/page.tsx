d38505c6bebe629e757363d66d244f6e8c3041ef
Farah Nazihah <52520854+farahnazihah@users.noreply.github.com>
2024-10-04 11:13:33 +0200

    feat: world traces (#3)
    * feat: world traces
    
    * fix: client side render WorldMap
    
    * chore: remove logger

---
+++ app/random/countries/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { IoInformationCircleOutline } from "react-icons/io5";
+
+const CustomWorldMap = dynamic(() => import("@/components/WorldMap"), {
+  ssr: false
+});
+
+const data = [
+  { country: "id", value: 0 }, // indonesia
+  { country: "my", value: 2 }, // malaysia
+  { country: "sg", value: 0.5 }, // singapore
+  { country: "cn", value: 1 }, // china
+  { country: "th", value: 0.5 }, // thailand
+  { country: "at", value: 1 }, // austria
+  { country: "cz", value: 1 }, // czech
+  { country: "be", value: 1 }, // belgium
+  { country: "fr", value: 1 }, // france
+  { country: "it", value: 1 }, // italy
+  { country: "sa", value: 1 }, // saudi arabia
+  { country: "va", value: 1 }, // vatican
+  { country: "jp", value: 1 }, // japan
+  { country: "tr", value: 0.5 }, // turkey
+  { country: "ch", value: 0 } // switzerland
+];
+
+export default function Traces() {
+  return (
+    <div className="py-12">
+      <h1 className="text-3xl font-bold text-tosca text-center">
+        {"Countries I've been to"}
+      </h1>
+
+      <div className="bg-tosca my-4 bg-opacity-20 rounded-lg p-4 flex flex-row gap-2 items-center md:hidden">
+        <IoInformationCircleOutline className="text-2xl" />
+        <p>{"😞 Use desktop to see it better."}</p>
+      </div>
+      <CustomWorldMap data={data} />
+    </div>
+  );
+}
